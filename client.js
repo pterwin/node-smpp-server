d904c2f9a673378cdfb44819095f427eb78215a3
Erwin Atuli <erwin@promotexter.com>
2015-12-08 02:56:03 +0800

    added simple client for testing purposes

--- client.js
+++ client.js
@@ -1,34 +1,35 @@
-// var client = require('./lib/SmppClient.js');
+var Client = require('./lib/SmppClient.js');
 var config = require('./config/config.js');
-console.log(config);
-// var client = new Client(config);
-// client.start();
 
 
+var client = new Client();
+client.addSession('localhost', 10070, 'YOUR_SYSTEM_ID', 'YOUR_PASSWORD' );
+client.addSession('localhost', 10070, 'YOUR_SYSTEM_ID', 'YOUR_PASSWORD' );
 
-var smpp = require('smpp');
-var session = smpp.connect('localhost', config.client.port);
-session.on('deliver_sm', function(pdu) {
-    console.log(pdu);
-    session.send(pdu.response({
-        command_status: 0
-    }));
-});
-session.bind_transceiver({
-    system_id : config.client.system_id,
-    password  : config.client.password
-}, function(pdu) {
-    console.log(pdu);
-    if (pdu.command_status == 0) {
-        // Successfully bound
-        session.submit_sm({
-            destination_addr: 'DESTINATION NUMBER',
-            short_message: 'Hello!'
-        }, function(pdu) {
-            if (pdu.command_status == 0) {
-                // Message successfully sent
-                console.log(pdu.message_id);
-            }
-        });
-    }
-});
+
+// var smpp = require('smpp');
+// var session = smpp.connect('localhost', config.client.port);
+// session.on('deliver_sm', function(pdu) {
+//     console.log(pdu);
+//     session.send(pdu.response({
+//         command_status: 0
+//     }));
+// });
+// session.bind_transceiver({
+//     system_id : config.client.system_id,
+//     password  : config.client.password
+// }, function(pdu) {
+//     console.log(pdu);
+//     if (pdu.command_status === 0) {
+//         // Successfully bound
+//         session.submit_sm({
+//             destination_addr: 'DESTINATION NUMBER',
+//             short_message: 'Hello!'
+//         }, function(pdu) {
+//             if (pdu.command_status === 0) {
+//                 // Message successfully sent
+//                 console.log(pdu.message_id);
+//             }
+//         });
+//     }
+// });
--- server.js
+++ server.js
@@ -2,4 +2,3 @@ var Server = require('./lib/SmppServer.js');
 var config = require('./config/config.js');
 var server = new Server(config);
 server.start();
-
